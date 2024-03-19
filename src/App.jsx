b082a75cecaf683e1a47f76ca921023f00e60e57
Lahir-Sai-Vignesh <lahirsaivignesh3@gmail.com>
2024-03-19 17:30:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './components/Header';
+import Search from './components/Search';
+import Note from './components/Note';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+        <Header />
+        <Search />
+        <Note />
+        <Footer />
+    </>
+  )
+}
+
+export default App
